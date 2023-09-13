7840f5399a8fcaaaa3ccf3313829a77390da5db1
Javier Andrés <54732923+jamg1906@users.noreply.github.com>
2023-09-12 19:44:00 -0600

    a simple stack with pop, push and peek methods

---
+++ aStack/Pila.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aStack
+{
+    public class humbleStack   
+    {
+        private Nodo? headNode = null;
+        private int count;
+        public int Count { get { return count; } }
+
+        public void Clear()
+        {
+            headNode = null;
+            count = 0;
+        }
+        public string Push(string value)
+        {
+            if(headNode == null)
+            {
+                headNode = new Nodo(value);
+                count++;
+                return headNode.valor;
+            }
+
+            Nodo newNode = new Nodo(value, headNode);
+            headNode = newNode;
+            count++;
+            return headNode.valor;
+
+        }
+        public string Pop()
+        {
+            if (headNode == null) { return "stack is empty."; }
+            if (headNode.nodoSiguiente == null) {
+                var temporal = headNode.valor;
+                Clear();
+                return  temporal; 
+            }
+            var temp = headNode.valor;
+            headNode = headNode.nodoSiguiente;
+            count--;
+            return temp;
+
+        }
+        public string Peek()
+        {
+            if (headNode == null) { return "stack is empty."; }
+            return headNode.valor;
+        }
+        public bool Exists(string key)
+        {
+            if (count == 0) { return false; }
+            Nodo temporal = headNode;
+            while (true)
+            {
+                if (temporal.valor == key) { return true; }
+
+                temporal = temporal.nodoSiguiente;
+
+                if(temporal == null) { return false; }
+            }
+        }
+
+    }
+
+    public class Nodo
+    {
+        public Nodo? nodoSiguiente = null;
+        public string valor;
+        public Nodo(string Valor, Nodo Siguiente)
+        {
+            valor = Valor;
+            nodoSiguiente = Siguiente;
+        }
+        public Nodo(string Valor)
+        {
+            valor = Valor;
+        }
+    }
+}
---
+++ aStack/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace aStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            humbleStack miPila = new humbleStack();
+            Console.WriteLine(miPila.Count);
+            Console.WriteLine(miPila.Peek());
+            Console.WriteLine(miPila.Pop());
+            miPila.Clear();
+
+            Console.WriteLine(miPila.Push("a"));
+            Console.WriteLine(miPila.Push("b"));
+            Console.WriteLine(miPila.Push("c"));
+            Console.WriteLine(miPila.Count);
+
+
+            Console.WriteLine(miPila.Exists("a"));
+            Console.WriteLine(miPila.Exists("b"));
+            Console.WriteLine(miPila.Exists("c"));
+            Console.WriteLine(miPila.Exists("j"));
+
+            Console.WriteLine(miPila.Pop());
+            Console.WriteLine(miPila.Pop());
+            Console.WriteLine(miPila.Pop());
+            Console.WriteLine(miPila.Pop());
+
+            Console.WriteLine(miPila.Push("jamg1906"));
+            Console.WriteLine(miPila.Peek());
+            Console.WriteLine(miPila.Count);
+            Console.WriteLine(miPila.Pop());
+
+
+            miPila.Clear();
+            Console.WriteLine(miPila.Push("c"));
+            Console.WriteLine(miPila.Exists("c"));
+            Console.WriteLine(miPila.Exists("j"));
+            Console.WriteLine(miPila.Count);
+
+            Console.WriteLine(miPila.Pop());
+            Console.WriteLine(miPila.Count);
+            Console.WriteLine(miPila.Exists("j"));
+
+
+
+
+
+
+
+
+        }
+    }
+}
